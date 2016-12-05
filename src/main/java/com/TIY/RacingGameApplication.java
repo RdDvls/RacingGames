6c8b5cc568e5aeea4155ff940435027b9e87dc6a
Clayton Strickland <cjstrickland5@gmail.com>
2016-12-05 16:37:26 -0500

    Made some drivers. Move to cars

--- src/main/java/com/TIY/RacingGameApplication.java
+++ src/main/java/com/TIY/RacingGameApplication.java
@@ -19,7 +19,27 @@ public class RacingGameApplication {
 	public void init(){
 		if(drivers.count() == 0){
 			Drivers driverKato = new Drivers("Kato",null, "Human");
+			Drivers driverMario = new Drivers("Mario", null, "Human");
+			Drivers driverYoshi = new Drivers("Yoshi",null,"Dinosaur");
+			Drivers driverDonkeyKong = new Drivers("Donkey", "Kong", "Primate");
+			Drivers driverMissDaisy = new Drivers("Hoke","Colbum","Human");
+			Drivers driverPope = new Drivers("Pope Francis", null,"Human");
+			Drivers driverBender = new Drivers("Bender", "Rodriguez","Robot");
+			Drivers driverFry = new Drivers("Philip", "Fry", "Human");
+			Drivers driverLeela = new Drivers("Turanga","Leela", "Mutant");
+			Drivers driverToonces = new Drivers("Toonces", "The Driving Cat", "Feline");
+			Drivers driverJLSimpson = new Drivers("Jimmy Lee", "Simpson", "Human");
 			drivers.save(driverKato);
+			drivers.save(driverMario);
+			drivers.save(driverYoshi);
+			drivers.save(driverDonkeyKong);
+			drivers.save(driverMissDaisy);
+			drivers.save(driverPope);
+			drivers.save(driverBender);
+			drivers.save(driverFry);
+			drivers.save(driverLeela);
+			drivers.save(driverToonces);
+			drivers.save(driverJLSimpson);
 		}
 	}
 }
